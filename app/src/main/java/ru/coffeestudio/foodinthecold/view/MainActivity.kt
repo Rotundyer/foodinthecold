1dcb61393f215fcd4a46cefc2aec87387082986b
Rotundyer <Rotundyerhello@gmail.com>
2021-01-22 17:05:19 +0300

    Счастье

--- app/src/main/java/ru/coffeestudio/foodinthecold/view/MainActivity.kt
+++ app/src/main/java/ru/coffeestudio/foodinthecold/view/MainActivity.kt
@@ -1,10 +1,13 @@
 package ru.coffeestudio.foodinthecold.view
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -26,8 +29,6 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
 
         if (savedInstanceState == null)
             tabManager.currentController = tabManager.navMainController
-
-//        supportActionBar?.hide()
     }
 
     override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
@@ -58,6 +59,16 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
 
         val searchItem = menu!!.findItem(R.id.menu_search)
         val searchView: SearchView = searchItem.actionView as SearchView
+
+        val open_sans = Typeface.createFromAsset(assets, "fonts/open_sans.ttf")
+        val open_sans_semibold = Typeface.createFromAsset(assets, "fonts/open_sans_semibold.ttf")
+        val searchText = searchView.findViewById(R.id.search_src_text) as TextView
+
+        searchText.apply {
+            typeface = open_sans
+            setTextColor(Color.WHITE)
+            setTextSize(1,20F)
+        }
 
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
